6ab6cfa9d855b06f5372851698d626e7b90e4033
naeemmerali <114533794+naeemmerali@users.noreply.github.com>
2023-01-01 22:54:07 -0800

    codewars solution of the day

---
+++ 8 Kyu/multiply.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b
+//}
+
+// return statement was missing
+
+function multiply(a, b){
+  return a * b
+}
